c3f9af55a3127e33cf8885e0d005e91d4af27341
taovilla <taovilla90@gmail.com>
2023-03-14 16:56:42 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener("click", function(){
+    btn.classList.toggle('open')
+    
+})
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/index.html",
+  "./js/script.js"      
+],
+  theme: {
+    screens:{
+      sm:"480px",
+      md: "768px",
+      lg: "976px",
+      xlg : "1440px"
+
+
+    },
+    extend: {
+      colors:{
+        BrightRed: "hsl(12, 88%, 59%)",
+        DarkBlue: "hsl(228, 39%, 23%)",
+        DarkGrayisBlue:"hsl(227, 12%, 61%)",
+        VeryDarkBlue: "hsl(233, 12%, 13%)",
+        VeryPaleRed: "hsl(13, 100%, 96%)",
+        VaryLightGray: "hsl(0, 0%, 98%)",
+      }
+    },
+  },
+  plugins: [],
+}
