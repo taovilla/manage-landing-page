431219a9763baf08eb25388875b5f05f5442c27d
taovilla <taovilla90@gmail.com>
2023-04-07 11:50:09 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './dist/index.html',
-    './src/**/*.{html,js}',   
+    "./build/*.html"  
 ],
   theme: {
     screens:{
